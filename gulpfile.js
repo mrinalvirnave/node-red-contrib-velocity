cf313fac0fb1f712b4e1824fec7c26dec402773b
MrinalVirnave <mrinal@virnave.com>
2016-09-18 19:02:18 -0700

    travis

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 /* File: gulpfile.js */
 
 // grab our gulp packages
-var gulp = require('gulp');
-var gutil = require('gulp-util');
+const gulp = require('gulp');
+const mocha = require('gulp-mocha');
+const gutil = require('gulp-util');
+const exit = require('gulp-exit');
+const plumber = require('gulp-plumber');
+const istanbul = require('gulp-istanbul');
+
+const path = require('path');
 
 // create a default task and just log a message
 gulp.task('default', function() {
-  return gutil.log('Gulp is running!')
+  return gutil.log('This module cannot be run standalone. Install it with node-red (http://nodered.org).');
 });
+
+gulp.task('test', () =>
+  gulp.src(path.join('test', '**', '*.js'), { read: false })
+    .pipe(mocha({ reporter: 'spec', timeout: 3000 }))
+    .pipe(exit())
+);
